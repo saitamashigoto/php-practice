34388519be2b96ebd57416693a5c0167697717b7
Saitama <piyushgoelshigoto@gmail.com>
2021-11-21 21:18:44 +0530

    started stack

--- 3-stacks.php
+++ 3-stacks.php
@@ -23,17 +23,27 @@ class ThreeStacks {
     }
 
     public function popFromFirst() {
-        if (!empty($this->stack)) {          
-            $length = count($this->stack);
-            for ($i = $length; $i >$this->top1+1 ; $i--) {
-                $this->stack[$i] = $this->stack[$i-1];
-            }
-            $this->top1++;
-            $this->top2++;
-            $this->top3++;
+        if ($this->size1 == 0) {
+            throw new \RuntimeException('Empty First stack');
         }
-        $this->stack[$this->top1] = $item;
-        $this->size1++; 
+        $item = $this->stack[$this->top1];
+        $length = count($this->stack);
+        for ($i = $this->top1; $i < $length-1 ; $i++) {
+            $this->stack[$i] = $this->stack[$i+1];
+        }
+        if ($this->top1 > 0) {
+            $this->top1--;
+        }
+        $this->top2--;
+        $this->top3--;
+        $this->size1--; 
+    }
+
+    public function peekFromFirst() {
+        if ($this->size1 == 0) {
+            throw new \RuntimeException('Empty First stack');
+        }
+        return $this->stack[$this->top1];
     }
     
     public function pushOnSecond($item) {
@@ -48,6 +58,29 @@ class ThreeStacks {
         $this->stack[$this->top2] = $item;
         $this->size2++; 
     }
+
+    public function popFromSecond() {
+        if ($this->size2 == 0) {
+            throw new \RuntimeException('Empty Second stack');
+        }
+        $item = $this->stack[$this->top2];
+        $length = count($this->stack);
+        for ($i = $this->top2; $i < $length-1 ; $i++) {
+            $this->stack[$i] = $this->stack[$i+1];
+        }
+        if ($this->top2 > 0) {
+            $this->top2--;
+        }
+        $this->top3--;
+        $this->size2--; 
+    }
+
+    public function peekFromSecond() {
+        if ($this->size2 == 0) {
+            throw new \RuntimeException('Empty Second stack');
+        }
+        return $this->stack[$this->top2];
+    }
     
     public function pushOnThird($item) {
         if (!empty($this->stack)) {          
@@ -60,4 +93,36 @@ class ThreeStacks {
         $this->stack[$this->top3] = $item;
         $this->size3++; 
     }
+
+    public function popFromThird() {
+        if ($this->size3 == 0) {
+            throw new \RuntimeException('Empty Third stack');
+        }
+        $item = $this->stack[$this->top3];
+        if ($this->top3 > 0) {
+            $this->top3--;
+        }
+        $this->size3--; 
+    }
+
+    public function peekFromThird() {
+        if ($this->size3 == 0) {
+            throw new \RuntimeException('Empty Third stack');
+        }
+        return $this->stack[$this->top3];
+    }
+
+    public function isEmptyFirst() {
+        return $this->size1 === 0;
+    }
+
+    public function isEmptySecond() {
+        return $this->size2 === 0;
+    }
+
+    public function isEmptyThird() {
+        return $this->size3 === 0;
+    }
 }
+
+$stack = new ThreeStacks();
